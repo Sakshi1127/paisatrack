cf326ced25b49a4f3a69a9fc5323031f3aa8aead
Sakshi <sakshimaheshwari19191@gmail.com>
2026-07-06 19:50:20 +0530

    fixing date issue

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,5 +1,10 @@
-const { Pool } = require('pg')
+const { Pool, types } = require('pg')
 require('dotenv').config()
+
+// Fix date timezone issue
+// By default pg converts dates to JS Date objects which shifts timezone
+// This tells pg to return dates as plain strings instead
+types.setTypeParser(1082, val => val)
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
