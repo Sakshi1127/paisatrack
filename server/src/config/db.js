ecc1f5dd868acdf026d3ac5c2d529f1309df39e9
Sakshi <sakshimaheshwari19191@gmail.com>
2026-09-05 13:10:37 +0530

    add ssl

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -8,6 +8,9 @@ types.setTypeParser(1082, val => val)
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 const testConnection = async () => {
